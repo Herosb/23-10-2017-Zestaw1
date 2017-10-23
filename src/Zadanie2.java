e1174227753832e3b4009e4a9dd9fa927a5bbff2
Student <Student@10.2.5.4>
2017-10-23 18:12:23 +0200

    zadanie 2

---
+++ src/Zadanie2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Zadanie2 {
+
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+		float a = reader.nextFloat();
+		float b = reader.nextFloat();
+		float c = reader.nextFloat();
+		if (a > b ) && (a > c){
+			if (a > c > b) {
+				if (c > a) {
+					System.out.println("najwieksza liczba"+)
+				}
+			}
+				
+		}
+	}
+
+}
